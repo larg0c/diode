9818edb0206399e3adb411de39cddbffc3448361
larg0c <60753761+larg0c@users.noreply.github.com>
2024-11-14 18:14:38 +0100

    Update

--- dyode.py
+++ dyode.py
@@ -1,42 +1,61 @@
 import os
-import dyode
+import hashlib
 import logging
+import subprocess
+import shlex
+import yaml
 from configparser import ConfigParser
 
+# Configuration du logging
 logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-def wait_for_file(params):
-    manifest_filename = 'manifest.cfg'
-    dyode.receive_file(manifest_filename, params['port'], params['ip'])
+# Charger la configuration YAML (exemple d'utilisation de yaml pour config.yaml)
+def load_yaml_config(file_path):
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
-    files = dyode.parse_manifest(manifest_filename)
-    if not files:
-        log.error("Aucun fichier reçu.")
-        return
+# Fonction de hachage des fichiers
+def hash_file(file):
+    BLOCKSIZE = 65536
+    hasher = hashlib.sha256()
+    with open(file, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+# Écriture du fichier manifeste au format .cfg (en utilisant ConfigParser pour un fichier INI)
+def write_manifest(files, manifest_filename):
+    config = ConfigParser()
+    config.add_section('Files')
+    for f in files:
+        config.set('Files', f, files[f])
+
+    with open(manifest_filename, 'w') as configfile:
+        config.write(configfile)
+
+# Lecture du fichier manifeste (format .cfg)
+def parse_manifest(file):
+    parser = ConfigParser()
+    parser.read(file)
+    if 'Files' not in parser.sections():
+        log.error("La section [Files] est manquante dans le fichier manifeste.")
+        return {}
     
-    for f, expected_hash in files.items():
-        temp_file = os.path.join(params['out'], os.path.basename(f))
-        dyode.receive_file(temp_file, params['port'], params['ip'])
+    return {item: value for item, value in parser.items('Files')}
 
-        if dyode.hash_file(temp_file) != expected_hash:
-            log.error(f"Checksum invalide pour le fichier {f}.")
-            os.remove(temp_file)
-        else:
-            log.info(f"Fichier {temp_file} reçu avec succès.")
+# Envoi de fichier via UDP
+def send_file(file, port_base, max_bitrate, ip):
+    command = f'udp-sender --async --fec 8x16/64 --max-bitrate {max_bitrate}m --mcast-rdv-addr {ip} --portbase {port_base} --autostart 1 --interface eth0 -f \'{file}\''
+    log.debug(command)
+    subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, shell=False).communicate()
 
-if __name__ == '__main__':
-    with open('config.yaml', 'r') as config_file:
-        config = ConfigParser()
-        config.read_file(config_file)
-
-    modules = config['modules']['file_transfer']
-    properties = {
-        'out': modules['out'],
-        'port': modules['port'],
-        'ip': config['dyode_out']['ip']
-    }
-
-    while True:
-        wait_for_file(properties)
+# Réception de fichier via UDP
+def receive_file(filepath, portbase, ip):
+    command = f'udp-receiver --nosync --mcast-rdv-addr {ip} --interface eth1 --portbase {portbase} -f \'{filepath}\''
+    log.debug(command)
+    subprocess.Popen(shlex.split(command), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
